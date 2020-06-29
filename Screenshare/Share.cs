e950b0d6661bdf9ebdc2a679fb6bcc37df1f452d
RobertsMaxwell <max031029@gmail.com>
2020-06-29 12:41:49 -0600

    Communication testing

---
+++ Screenshare/Share.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Screenshare
+{
+    static class Share
+    {
+        public static string TEST_ADDRESS = "192.168.1.127";
+        public static int TEST_PORT = 49152;
+
+        public static void InitiateTCPConnect()
+        {
+            var connection = new TcpClient();
+            connection.Connect(new IPEndPoint(IPAddress.Parse(TEST_ADDRESS), TEST_PORT));
+            StreamWriter writer = new StreamWriter(connection.GetStream());
+            writer.WriteLine("Hello");
+        }
+    }
+}
